85fd840641ccd43621f53331e3f81eb2f6331837
Filip Chalupa <chalupa.filip@gmail.com>
2020-11-10 18:23:32 +0100

    Řešení: úloha 2

--- src/Uloha2/index.jsx
+++ src/Uloha2/index.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // Zadání 1: Převeď `cislo` na stavovou proměnnou.
 // Zadání 2: Nastav, aby jednotlivá tlačítka měnila stav. +1 přičítalo jedničku, +5 přičítalo pětku, vynulovat nastavovalo na nulu apod.
 
 const Pocitadlo = () => {
-	const cislo = 0
+	const [cislo, setCislo] = useState(0)
 
 	return (
 		<>
 			<h3>Počítadlo: {cislo}</h3>
-			<button>-5</button>
-			<button>-1</button>
-			<button>vynulovat</button>
-			<button>+1</button>
-			<button>+5</button>
+			<button onClick={() => setCislo(cislo - 5)}>-5</button>
+			<button onClick={() => setCislo(cislo - 1)}>-1</button>
+			<button onClick={() => setCislo(0)}>vynulovat</button>
+			<button onClick={() => setCislo(cislo + 1)}>+1</button>
+			<button onClick={() => setCislo(cislo + 5)}>+5</button>
 		</>
 	)
 }
