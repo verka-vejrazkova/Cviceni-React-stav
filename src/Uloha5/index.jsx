7474c70a3d7deb074ba561e697118f0bab8bbcbe
Filip Chalupa <chalupa.filip@gmail.com>
2020-11-10 18:33:41 +0100

    Úloha 5: nápověda

--- src/Uloha5/index.jsx
+++ src/Uloha5/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './hamburger.css'
 
-// Zadání 1: Přichystej si stavovou proměnnou, ve které bude uloženo, jestli je menu otevřené nebo nikoliv.
+// Zadání 1: Přichystej si stavovou proměnnou s výchozí hodnotou `false`, ve které bude uloženo, jestli je menu otevřené nebo nikoliv.
 // Zadání 2: Po kliknutí na button změň stav z `true` na `false` nebo obráceně.
 // Zadání 3: Nerenderuj `ul`, pokud je stav `false`.
 // Zadání 4: Pokud je menu zavřené nech mu jen třídu `hamburger`. Pro otevřené přidej navíc `hamburger--otevrene`.
