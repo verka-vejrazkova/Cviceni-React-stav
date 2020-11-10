e0ca6555da5cc4f6cb9ddc19a41256300a6445d1
Filip Chalupa <chalupa.filip@gmail.com>
2020-11-10 18:50:34 +0100

    Bonus 1: typo

--- src/ZaverecnyBonus1/index.jsx
+++ src/ZaverecnyBonus1/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 // Zadání 1: Pomocí události `change` na vstupním políčku a vlastnosti `event.target.value` ukládej do stavu, co uživatel napsal.
 // Zadání 2: Mezi tagy `<b></b>` vlož napsaný text.
 // Zadání 3. V dalším odstavci `<p>` zobraz, kolik má text znaků.
-// Zadání 4. Poslední odstavec zobraz pouze v případě, že má text aspoň 8 znaků.
+// Zadání 4. Poslední odstavec zobraz pouze v případě, že je text kratší než 8 znaků.
 
 export const ZaverecnyBonus1 = () => {
 	return (
