99146c6c80d03eccf5edc01fcd4b2de6c075ab1d
Filip Chalupa <chalupa.filip@gmail.com>
2020-11-11 00:18:59 +0100

    Řešení: úloha 8

--- src/Uloha8/index.jsx
+++ src/Uloha8/index.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // Zadání 1: Vytvoř stavovou proměnnou s výchozí hodnotou `'možná'`.
 // Zadání 2: Proměnnou vypiš místo `ano/ne/možná`.
 // Zadání 3: Po kliknutí na tlačítko změň `možná` na `ano`, `ano` na `ne`, případně `ne` na `možná`.
 
 export const Uloha8 = () => {
+	const [prsi, setPrsi] = useState('možná')
+	const zmen = () => {
+		if (prsi === 'možná') {
+			setPrsi('ano')
+		} else if (prsi === 'ano') {
+			setPrsi('ne')
+		} else {
+			setPrsi('možná')
+		}
+	}
+
 	return (
 		<>
-			<h3>Prší v Brně: ano/ne/možná</h3>
-			<button>změnit</button>
+			<h3>Prší v Brně: {prsi}</h3>
+			<button onClick={zmen}>změnit</button>
 		</>
 	)
 }
