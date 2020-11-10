78f04b46b14ba473947c8052d78b8e5ae8f095b4
Filip Chalupa <chalupa.filip@gmail.com>
2020-11-10 18:35:53 +0100

    Řešení: úloha 5

--- src/Uloha5/index.jsx
+++ src/Uloha5/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './hamburger.css'
 
 // Zadání 1: Přichystej si stavovou proměnnou s výchozí hodnotou `false`, ve které bude uloženo, jestli je menu otevřené nebo nikoliv.
@@ -7,27 +7,35 @@ import './hamburger.css'
 // Zadání 4: Pokud je menu zavřené nech mu jen třídu `hamburger`. Pro otevřené přidej navíc `hamburger--otevrene`.
 
 export const Uloha5 = () => {
+	const [otevrene, setOtevrene] = useState(false)
+
 	return (
 		<>
-			<button className="hamburger" aria-label="menu">
+			<button
+				className={otevrene ? 'hamburger hamburger--otevrene' : 'hamburger'}
+				aria-label="menu"
+				onClick={() => setOtevrene(!otevrene)}
+			>
 				<span></span>
 				<span></span>
 				<span></span>
 			</button>
-			<ul>
-				<li>
-					<a href="#o-nas">O nás</a>
-				</li>
-				<li>
-					<a href="#co-delame">Co děláme</a>
-				</li>
-				<li>
-					<a href="#kontakty">Kontakty</a>
-				</li>
-				<li>
-					<a href="#cenik">Ceník</a>
-				</li>
-			</ul>
+			{otevrene && (
+				<ul>
+					<li>
+						<a href="#o-nas">O nás</a>
+					</li>
+					<li>
+						<a href="#co-delame">Co děláme</a>
+					</li>
+					<li>
+						<a href="#kontakty">Kontakty</a>
+					</li>
+					<li>
+						<a href="#cenik">Ceník</a>
+					</li>
+				</ul>
+			)}
 		</>
 	)
 }
