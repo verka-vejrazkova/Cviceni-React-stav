b6ef0550ff812fb9d9f103ad7cae5566086f45e9
Filip Chalupa <chalupa.filip@gmail.com>
2020-11-10 18:47:59 +0100

    Řešení: úloha 7

--- src/Uloha7/index.jsx
+++ src/Uloha7/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ukazatel-uspechu.css'
 
 // Zadání 1: Použij prop `barva` ve style atributu.
@@ -7,18 +7,22 @@ import './ukazatel-uspechu.css'
 // Zadání 4. Nastav prvku `ukazatel-uspechu__postup` šířku podle stavové proměnné.
 
 const UkazatelPokroku = ({ barva }) => {
+	const [stav, setStav] = useState(0)
+
 	return (
 		<div className="ukazatel-uspechu">
 			<div className="ukazatel-uspechu__ramecek">
 				<div
 					className="ukazatel-uspechu__postup"
 					style={{
-						width: '40%',
-						backgroundColor: 'red',
+						width: `${stav}%`,
+						backgroundColor: barva,
 					}}
 				></div>
 			</div>
-			<button>postoupit o 10 %</button>
+			<button onClick={() => setStav(Math.min(100, stav + 10))}>
+				postoupit o 10 %
+			</button>
 		</div>
 	)
 }
