17a88969fde60cb74670f8d7ab8947c41f8dec68
Filip Chalupa <chalupa.filip@gmail.com>
2020-11-10 23:57:35 +0100

    Řešení: bonus 4

--- src/ZaverecnyBonus4/index.jsx
+++ src/ZaverecnyBonus4/index.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // Zadání: Pomocí dvou stavových proměnných s výchozí hodnotou 0, které se budou měnit podle vstupních políček, vypiš do `<output></output>` jejich součet.
 
 export const ZaverecnyBonus4 = () => {
+	const [vstup1, setVstup1] = useState(0)
+	const [vstup2, setVstup2] = useState(0)
+
 	return (
 		<>
-			<input type="number" defaultValue="0" /> +{' '}
-			<input type="number" defaultValue="0" /> = <output>0</output>
+			<input
+				onChange={(event) => {
+					setVstup1(Number(event.target.value))
+				}}
+				type="number"
+				defaultValue="0"
+			/>{' '}
+			+{' '}
+			<input
+				onChange={(event) => {
+					setVstup2(Number(event.target.value))
+				}}
+				type="number"
+				defaultValue="0"
+			/>{' '}
+			= <output>{vstup1 + vstup2}</output>
 		</>
 	)
 }
