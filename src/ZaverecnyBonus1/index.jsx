7f7f0c82df3bb2387b8abfb3f7f1a25b44eef02c
Filip Chalupa <chalupa.filip@gmail.com>
2020-11-10 18:49:51 +0100

    Řešení bonus 1

--- src/ZaverecnyBonus1/index.jsx
+++ src/ZaverecnyBonus1/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // Zadání 1: Pomocí události `change` na vstupním políčku a vlastnosti `event.target.value` ukládej do stavu, co uživatel napsal.
 // Zadání 2: Mezi tagy `<b></b>` vlož napsaný text.
@@ -6,16 +6,17 @@ import React from 'react'
 // Zadání 4. Poslední odstavec zobraz pouze v případě, že je text kratší než 8 znaků.
 
 export const ZaverecnyBonus1 = () => {
+	const [vstup, setVstup] = useState('')
 	return (
 		<>
 			<label>
-				Napiš něco: <input />
+				Napiš něco: <input onChange={(event) => setVstup(event.target.value)} />
 			</label>
 			<p>
-				Do políčka výše uživatel napsal: <b>@TODO</b>
+				Do políčka výše uživatel napsal: <b>{vstup}</b>
 			</p>
-			<p>Počet znaků: 0</p>
-			<p>Jako heslo by text neobstál.</p>
+			<p>Počet znaků: {vstup.length}</p>
+			{vstup.length < 8 && <p>Jako heslo by text neobstál.</p>}
 		</>
 	)
 }
